645f2818aba0204a54999ea491eb4a0cd8b5fc68
Elena <eporubin@gmail.com>
2023-08-09 17:55:40 +0200

    Grouped by snippet, elem_hide_pseudo and blocking and specified to create new list only if the list is not empty

--- main.py
+++ main.py
@@ -10,27 +10,31 @@ def convert_response(response):
 def classify_filter(filter_type):
     if '#$#' in filter_type:
         return 'snippet'
+    elif '#@#' in filter_type:
+        return 'elem_hide_pseudo'
     elif '#?#' in filter_type:
         return 'elem_hide_pseudo'
-    elif '##' in filter_type:
-        return 'elem_hide'
-    elif "#@#" in filter_type:
-        return 'allowlist_elem_hide'
-    elif filter_type.startswith('|'):
-        return 'network_request_block'
-    elif filter_type.startswith('@@|'):
-        return 'allowlist_network_request'
+    # elif '##' in filter_type:
+    #     return 'elem_hide'
+    # elif "#@#" in filter_type:
+    #     return 'allowlist_elem_hide'
+    # elif filter_type.startswith('|'):
+    #     return 'network_request_block'
+    # elif filter_type.startswith('@@|'):
+    #     return 'allowlist_network_request'
     else:
         return 'blocking_filters'
 
 #declare dictionary of filter types
 filter_categories = {
+    # snippets only
     "snippet": [],
+    # only filters with #?#, #@#
     "elem_hide_pseudo": [],
-    "elem_hide":  [],
-    "allowlist_elem_hide": [],
-    "network_request_block": [],
-    "allowlist_network_request": [],
+    # "elem_hide":  [],
+    # "allowlist_elem_hide": [],
+    # "network_request_block": [],
+    # "allowlist_network_request": [],
     "blocking_filters": []
     }
 
@@ -45,12 +49,13 @@ if __name__ == '__main__':
         filter_type = classify_filter(list_line)
         filter_categories[filter_type].append(list_line)
     for category in filter_categories:
-        f = open(f'{name_prefix}_{category}.txt','w')
-        if (category !='blocking_filters'):
+        # create the list only if it's not empty
+        if len(filter_categories[category]) > 0:
+            f = open(f'{name_prefix}_{category}.txt','w')
             f.write(list_to_filter[0] + "\n")
-        lines_to_print = filter_categories[category]
-        f.writelines([x + "\n" for x in lines_to_print])
-        f.close()
+            lines_to_print = filter_categories[category]
+            f.writelines([x + "\n" for x in lines_to_print])
+            f.close()
 
 
 
